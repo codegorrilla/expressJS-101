70e120a9e31ab937eb8cce7cfbf070ae16927ead
codegorrilla <code.gorrilla@gmail.com>
2026-03-15 09:19:24 +0530

    modified Schema to add multiple validators

--- models/Person.js
+++ models/Person.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
+// const personSchema = new mongoose.Schema({
+// 	name: String,
+// 	age: Number,
+// 	email: String,
+// });
+
 const personSchema = new mongoose.Schema({
-	name: String,
-	age: Number,
-	email: String,
+	name: { type: String, required: true }, //multiple validators
+	age: { type: Number, required: true },
+	email: { type: String, required: true, unique: true },
 });
 
 export const Person = mongoose.model("Person", personSchema); //model name , model schema
