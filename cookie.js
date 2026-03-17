04cb109afd8beb62377a680fdf3bb6c67e0f9b85
Sanjib Adhya <code.gorrilla@gmail.com>
2026-03-17 10:52:18 +0530

    started learning authentication basics

--- cookie.js
+++ cookie.js
@@ -1,10 +1,20 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
+
 const port = 3002;
 
+app.use(express.json());
 app.use(cookieParser());
+app.use(
+	session({
+		secret: "sample-secret",
+		resave: false,
+		saveUninitialized: false,
+	}),
+);
 
 app.get("/", (req, res) => {
 	//res.cookie("name", "express-app", { maxAge: 360000 });
@@ -20,6 +30,38 @@ app.get("/fetch", (req, res) => {
 app.get("/remove-cookie", (req, res) => {
 	res.clearCookie("name");
 	res.send("Cookie cleared.");
+});
+
+const users = [];
+
+app.post("/register", async (req, res) => {
+	const { username, password } = req.body;
+	users.push({
+		username,
+		password,
+	});
+
+	res.send("User registered.");
+});
+
+app.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	const user = users.find((u) => u.username === username);
+	if (!user || password !== user.password) {
+		return res.send("Not authorized");
+	}
+
+	req.session.user = user;
+
+	res.send("User logged in.");
+});
+
+app.get("/dashboard", (req, res) => {
+	if (!req.session.user) {
+		return res.send("Unauthorized");
+	}
+
+	res.send(`Welcome ${req.session.user.username}`);
 });
 
 app.listen(port, () => {
